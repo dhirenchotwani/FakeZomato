d7403db2bd8b7d287007a1e5a5189d460d665965
Dhiren Chotwani <dhirenchotwani@gmail.com>
2019-06-30 01:16:14 +0530

    odered screen completed

--- screens/orderScreens/DeliveryContent.js
+++ screens/orderScreens/DeliveryContent.js
@@ -5,6 +5,7 @@ import {
     Platform,
     ScrollView,
     StyleSheet,
+    ImageBackground,
     Text,
     TouchableOpacity,
     View,
@@ -12,7 +13,7 @@ import {
 } from 'react-native';
 
 import DeliveryHotelDetails from './DeliveryHotelDetails';
-import {Ionicons} from "@expo/vector-icons";
+import {Ionicons, MaterialIcons} from "@expo/vector-icons";
 import {responsiveHeight,responsiveWidth,responsiveFontSize} from "react-native-responsive-dimensions";
 import Colors from '../../constants/Colors';
 export default class DeliveryContent extends  React.Component{
@@ -20,27 +21,138 @@ export default class DeliveryContent extends  React.Component{
         console.log("Inside Delivery Content");
         return (
             <View style={styles.container}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <View style={styles.deals}>
+                        <ImageBackground source={{uri:"https://i2.wp.com/www.lootpur.in/wp-content/uploads/2019/04/images-78-323493120..jpg?resize=300%2C190&ssl=1"}}  style={styles.dealImages}>
+                            <Text style={styles.dealText}>Zomato deal text</Text>
+                        </ImageBackground>
+                    </View>
+                    <View style={styles.deals}>
+                        <ImageBackground source={{uri:"https://i2.wp.com/www.lootpur.in/wp-content/uploads/2019/04/images-78-323493120..jpg?resize=300%2C190&ssl=1"}}  style={styles.dealImages}>
+                            <Text style={styles.dealText}>Zomato deal text</Text>
+                        </ImageBackground>
+                    </View>
+                    <View style={styles.deals}>
+                        <ImageBackground source={{uri:"https://i2.wp.com/www.lootpur.in/wp-content/uploads/2019/04/images-78-323493120..jpg?resize=300%2C190&ssl=1"}}  style={styles.dealImages}>
+                            <Text style={styles.dealText}>Zomato deal text</Text>
+                        </ImageBackground>
+                    </View>
+                    <View style={styles.deals}>
+                        <ImageBackground source={{uri:"https://i2.wp.com/www.lootpur.in/wp-content/uploads/2019/04/images-78-323493120..jpg?resize=300%2C190&ssl=1"}}  style={styles.dealImages}>
+                            <Text style={styles.dealText}>Zomato deal text</Text>
+                        </ImageBackground>
+                    </View>
+                </ScrollView>
                 <View style={styles.search}>
-                    <TextInput placeholder={'Search for restauranrs,dishes...'} style={styles.searchInput}/>
+                    <View style={styles.searchBar}>
+                        <MaterialIcons name={'search'} color={'gray'} size={responsiveFontSize(3)}/>
+                        <TextInput placeholder={'Search for restauranrs,dishes...'}  placeholderTextColor={'#333'} style={styles.searchInput}/>
+                    </View>
                     <TouchableOpacity style={styles.filter}>
                         <Ionicons name={'md-options'} color={'black'}
-                                       size={responsiveFontSize(3)}/>
+                                  size={responsiveFontSize(3)}/>
                     </TouchableOpacity>
                 </View>
-                <TouchableOpacity title={'Phase 1'}
-                                  onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
-                                  style={styles.infoContainer}>
+                    <ScrollView horizontal={false}>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                      style={styles.infoContainer}>
 
-                    <Image style={styles.image}
-                           source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
-                    <View styles={styles.hotelInfoContainer}>
-                        <Text>Hotel Name</Text>
-                        <Text>Hotel Address</Text>
-                        <Text>Average Cost</Text>
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
 
-                    </View>
+                        </View>
 
-                </TouchableOpacity>
+                    </TouchableOpacity>
+                        <TouchableOpacity title={'Phase 1'}
+                                          onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                          style={styles.infoContainer}>
+
+                            <Image style={styles.image}
+                                   source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                            <View styles={styles.hotelInfoContainer}>
+                                <Text>Hotel Name</Text>
+                                <Text>Hotel Address</Text>
+                                <Text>Average Cost</Text>
+
+                            </View>
+
+                        </TouchableOpacity>
+                        <TouchableOpacity title={'Phase 1'}
+                                          onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                          style={styles.infoContainer}>
+
+                            <Image style={styles.image}
+                                   source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                            <View styles={styles.hotelInfoContainer}>
+                                <Text>Hotel Name</Text>
+                                <Text>Hotel Address</Text>
+                                <Text>Average Cost</Text>
+
+                            </View>
+
+                        </TouchableOpacity>
+                        <TouchableOpacity title={'Phase 1'}
+                                          onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                          style={styles.infoContainer}>
+
+                            <Image style={styles.image}
+                                   source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                            <View styles={styles.hotelInfoContainer}>
+                                <Text>Hotel Name</Text>
+                                <Text>Hotel Address</Text>
+                                <Text>Average Cost</Text>
+
+                            </View>
+
+                        </TouchableOpacity>
+                        <TouchableOpacity title={'Phase 1'}
+                                          onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                          style={styles.infoContainer}>
+
+                            <Image style={styles.image}
+                                   source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                            <View styles={styles.hotelInfoContainer}>
+                                <Text>Hotel Name</Text>
+                                <Text>Hotel Address</Text>
+                                <Text>Average Cost</Text>
+
+                            </View>
+
+                        </TouchableOpacity>
+                        <TouchableOpacity title={'Phase 1'}
+                                          onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                          style={styles.infoContainer}>
+
+                            <Image style={styles.image}
+                                   source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                            <View styles={styles.hotelInfoContainer}>
+                                <Text>Hotel Name</Text>
+                                <Text>Hotel Address</Text>
+                                <Text>Average Cost</Text>
+
+                            </View>
+
+                        </TouchableOpacity>
+                        <TouchableOpacity title={'Phase 1'}
+                                          onPress={() => this.props.navigation.navigate('DeliveryHotelDetails')}
+                                          style={styles.infoContainer}>
+
+                            <Image style={styles.image}
+                                   source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                            <View styles={styles.hotelInfoContainer}>
+                                <Text>Hotel Name</Text>
+                                <Text>Hotel Address</Text>
+                                <Text>Average Cost</Text>
+
+                            </View>
+
+                        </TouchableOpacity>
+                    </ScrollView>
             </View>
         );
     }
@@ -56,35 +168,59 @@ const styles = StyleSheet.create({
     },
     infoContainer:{
         flexDirection:'row',
+
         paddingHorizontal:responsiveWidth(4),
-        paddingVertical:responsiveHeight(4)
+        paddingVertical:responsiveHeight(2)
     },
     image:{
         width:responsiveWidth(25),
         height:responsiveWidth(35)
     },
     search:{
-        paddingHorizontal: responsiveWidth(4),
+        justifyContent:'space-between',
+        paddingHorizontal:responsiveWidth(2),
         paddingVertical: responsiveHeight(1.5),
         flexDirection: 'row',
         alignItems:'center',
     },
     filter:{
-            backgroundColor:Colors.iconColor,
+            backgroundColor:'#eee',
 
         paddingVertical:responsiveWidth(1.2),
         paddingHorizontal:responsiveHeight(1.2),
         borderRadius:responsiveHeight(1.2)
     },
     searchInput:{
+       flex:1,
+
+
+        fontSize:responsiveFontSize(2),
+
+    },
+    searchBar:{
+        flexDirection:'row',
         height:responsiveHeight(4.5),
-        width:"87%",
+        width:responsiveWidth(75),
         backgroundColor:'#eee',
         borderRadius:responsiveHeight(1),
         paddingVertical:responsiveHeight(0.3),
         paddingHorizontal:responsiveWidth(2),
-        fontSize:responsiveFontSize(2),
-        marginRight:responsiveWidth(2.5),
+
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    deals:{
+        paddingHorizontal:responsiveWidth(2),
+        paddingVertical: responsiveHeight(1.5),
+    },
+    dealImages:{
+        width:responsiveWidth(40),
+        height:responsiveWidth(40),
+
+    },
+    dealText:{
+        color:'white',
+        paddingHorizontal:responsiveWidth(2),
     }
 
 });
--- screens/orderScreens/SelfPickupContent.js
+++ screens/orderScreens/SelfPickupContent.js
@@ -1,34 +1,217 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
-    Button,
+    Button, Image, ImageBackground, ScrollView,
     StyleSheet,
-    Text,
+    Text, TextInput, TouchableOpacity,
     View,
 } from 'react-native';
 
 import SelfPickupDetails from './SelfPickupDetails';
+import {Ionicons, MaterialIcons} from "@expo/vector-icons";
+import {responsiveFontSize, responsiveHeight, responsiveWidth} from "react-native-responsive-dimensions";
 
 export default class SelfPickupContent extends  React.Component{
     render() {
         console.log("Inside SelfPickup Content");
         return (
             <View style={styles.container}>
-                <Button title={'Phase 1'} onPress={()=> this.props.navigation.navigate('SelfPickupDetails')}/>
+                <View style={styles.newFeatureContainer}>
+                <Image source={require('../../assets/images/selfpickup.jpg')} style={styles.newFeature}/>
+                </View>
+                <View style={styles.search}>
+                    <View style={styles.searchBar}>
+                        <MaterialIcons name={'search'} color={'gray'} size={responsiveFontSize(3)}/>
+                        <TextInput placeholder={'Search for restauranrs,dishes...'}  placeholderTextColor={'#333'} style={styles.searchInput}/>
+                    </View>
+                    <TouchableOpacity style={styles.filter}>
+                        <Ionicons name={'md-options'} color={'black'}
+                                  size={responsiveFontSize(3)}/>
+                    </TouchableOpacity>
+                </View>
+                <ScrollView horizontal={false}>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('Sel')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('SelfPickupDetails')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('SelfPickupDetails')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('SelfPickupDetails')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('SelfPickupDetails')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('SelfPickupDetails')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity title={'Phase 1'}
+                                      onPress={() => this.props.navigation.navigate('SelfPickupDetails')}
+                                      style={styles.infoContainer}>
+
+                        <Image style={styles.image}
+                               source={{uri: "https://image.shutterstock.com/image-vector/smiling-pizza-delivery-courier-boy-260nw-1023156019.jpg"}}/>
+                        <View styles={styles.hotelInfoContainer}>
+                            <Text>Hotel Name</Text>
+                            <Text>Hotel Address</Text>
+                            <Text>Average Cost</Text>
+
+                        </View>
+
+                    </TouchableOpacity>
+                </ScrollView>
             </View>
         );
     }
 }
 
-// DeliveryContent.navigationOptions = {
+// SelfPickupDetails.navigationOptions = {
 //     header: null,
 // };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    infoContainer:{
+        flexDirection:'row',
+
+        paddingHorizontal:responsiveWidth(4),
+        paddingVertical:responsiveHeight(2)
+    },
+    image:{
+        width:responsiveWidth(25),
+        height:responsiveWidth(35)
+    },
+    search:{
+        justifyContent:'space-between',
+        paddingHorizontal:responsiveWidth(2),
+        paddingVertical: responsiveHeight(1.5),
+        flexDirection: 'row',
+        alignItems:'center',
+    },
+    filter:{
+        backgroundColor:'#eee',
+
+        paddingVertical:responsiveWidth(1.2),
+        paddingHorizontal:responsiveHeight(1.2),
+        borderRadius:responsiveHeight(1.2)
+    },
+    searchInput:{
+        flex:1,
+
+
+        fontSize:responsiveFontSize(2),
+
+    },
+    searchBar:{
+        flexDirection:'row',
+        height:responsiveHeight(4.5),
+        width:responsiveWidth(75),
+        backgroundColor:'#eee',
+        borderRadius:responsiveHeight(1),
+        paddingVertical:responsiveHeight(0.3),
+        paddingHorizontal:responsiveWidth(2),
+
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    deals:{
+        paddingHorizontal:responsiveWidth(2),
+        paddingVertical: responsiveHeight(1.5),
+    },
+    dealImages:{
+        width:responsiveWidth(40),
+        height:responsiveWidth(40),
 
 
     },
+    dealText:{
+        color:'white',
+        paddingHorizontal:responsiveWidth(2),
+    },
+    newFeature:{
+        width:340,
+        height:180,
+        borderRadius:10,
+    },
+    newFeatureContainer:{
+        justifyContent:'center',
+        alignItems:'center',
+        paddingVertical:responsiveWidth(2),
+        paddingHorizontal:responsiveWidth(2),
+
+
+    }
 
 });
+
